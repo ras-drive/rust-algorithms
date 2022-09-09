b70b86fd28c5d0a979f24e517e2756bea5a355dc
Sarah <rasdrive4165@protonmail.com>
2022-09-09 00:32:12 -0500

    added more tests and a bench to bubble sort

--- sort/bubble/src/lib.rs
+++ sort/bubble/src/lib.rs
@@ -1,4 +1,5 @@
 #![feature(is_sorted)]
+#![feature(test)]
 
 pub fn bubble_sort(mut vec: Vec<i32>) -> Vec<i32> {
     loop {
@@ -25,6 +26,7 @@ pub fn bubble_sort(mut vec: Vec<i32>) -> Vec<i32> {
 #[cfg(test)]
 mod tests {
     use super::*;
+    use rand::Rng;
 
     #[test]
     fn test_simple_list() {
@@ -32,5 +34,46 @@ mod tests {
         let list = bubble_sort(list);
 
         assert!(list.is_sorted());
+    }
+
+    #[test]
+    fn list_with_negatives() {
+        let list = vec![28, 6, -2, 18, -69];
+        let list = bubble_sort(list);
+
+        assert!(list.is_sorted());
+    }
+
+    #[test]
+    fn empty_list() {
+        let list = vec![];
+        let list = bubble_sort(list);
+
+        assert!(list.is_sorted());
+    }
+
+    #[test]
+    fn one_element_list() {
+        let list = vec![1];
+        let list = bubble_sort(list);
+
+        assert!(list.is_sorted());
+    }
+
+    extern crate test;
+    use test::Bencher;
+
+    #[bench]
+    fn bench_100_item_list(b: &mut Bencher) {
+        let mut rng = rand::thread_rng();
+        let mut list = vec![];
+        
+        for _ in 0..100 {
+            list.push(rng.gen());
+        }
+
+        b.iter(|| {
+        test::black_box(bubble_sort(list.clone()));
+    });
     }
 }
